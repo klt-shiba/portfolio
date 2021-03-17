bdefc61427488d73bc208fa32e4701696686e704
Colin Tseng <klt.aus@yahoo.com>
2021-03-17 22:18:07 +1100

    Completed assignment

--- script.js
+++ script.js
@@ -2,19 +2,26 @@ window.onload=function() {
 
     let navBar = document.getElementById("nav-header");
     let navBarLinks = navBar.getElementsByTagName('a');
-
     let modal = document.getElementById("my-modal");
 
-      function showModal() {
-        modal.classList.add("show-modal")
-      }
-      function addBlur() {
+    function showModal() {
+        modal.classList.add("show-modal");
+    }
+
+    function addBlur() {    
         let sectionContainer = document.getElementsByTagName('section');
         for (i = 0; i < sectionContainer.length; i++) {
             sectionContainer[i].classList.add('bg-blur');
         }
-      }
-      navBarLinks.addEventListener('click', showModal(), addBlur())
-     
+        navBar.classList.add('bg-blur');
+    }
       
+    for (var i = 0 ; i < navBarLinks.length; i++) {
+        navBarLinks[i].addEventListener('click', function (e) {
+            addBlur(), 
+            showModal()
+        });
+    }
+
+     
 }
