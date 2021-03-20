bb8d4012520713da4890a88dcc0a8368e714e856
Colin Tseng <klt.aus@yahoo.com>
2021-03-20 17:56:00 +1100

    refactor js code

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ window.onload=function() {
     let modal = document.getElementById("my-modal");
     let modalButton = document.getElementById("modal-close-buttn");
 
-    function showModal() {
-        modal.classList.add("show-modal");
+    function toggleModal() {
+       ((modal.classList.contains("show-modal")) ? 
+       (modal.classList.remove("show-modal")) : 
+       (modal.classList.add("show-modal")));
     }
-    function hideModal() {
-        modal.classList.remove("show-modal");
-    }
+
     function toggleblur() {
         let sectionContainer = document.getElementsByTagName('section'); 
         for (i = 0; i < sectionContainer.length; i++) { 
@@ -21,53 +21,27 @@ window.onload=function() {
             
         }
     }
-    // function addBlur() {    
-    //     let sectionContainer = document.getElementsByTagName('section');
-    //     for (i = 0; i < sectionContainer.length; i++) {
-    //         sectionContainer[i].classList.add('bg-blur');
-    //     }
-    //     navBar.classList.add('bg-blur');
-    // }
-    // function removeBlur() {    
-    //     let sectionContainer = document.getElementsByTagName('section');
-    //     for (i = 0; i < sectionContainer.length; i++) {
-    //         sectionContainer[i].classList.remove('bg-blur');
-    //     }
-    //     navBar.classList.remove('bg-blur');
-    // }
-    
-    function lockScroll() {
-        let body = document.getElementById('klt-portfolio');
-        body.classList.add('no-scroll');
-    }
-    function unlockScroll() {
-        let body = document.getElementById('klt-portfolio');
-        body.classList.remove('no-scroll');
-    }
-    
-    
-    function triggerModal() {
-        showModal();
-        lockScroll();
-        toggleblur();
-    }
+    function toggleScroll() {
+        let body = document.getElementById('klt-portfolio'); 
 
-    function triggerModalOff() {
-        unlockScroll();
+        ((body.classList.contains("no-scroll")) ? 
+        (body.classList.remove("no-scroll")) : 
+        (body.classList.add("no-scroll")));
+    }
+    function toggleModalExperience() {
+        toggleModal()
+        toggleScroll()
         toggleblur();
-        hideModal();
     }
 
     for (var i = 0 ; i < navBarLinks.length; i++) {
         navBarLinks[i].addEventListener('click', function (e) {
-            triggerModal();
+            toggleModalExperience()
         });
     }
 
-
     modalButton.addEventListener('click', function(e) {
-        triggerModalOff();
+        toggleModalExperience()
     })
-
      
 }
